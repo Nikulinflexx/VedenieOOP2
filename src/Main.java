20be33d8d7bcfb810b09790905065a19fb9f25fc
Nikita <nickita.nikulin2015@yandex.ru>
2024-08-20 23:23:56 +0300

    modified

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
+        int bmi;
         float weightKg = 73.312f;
         float heightM = 1.835f;
-        float bmi = service.calculate(weightKg, heightM);
+        bmi = (int) service.calculate(weightKg, heightM);
         System.out.println(bmi);
     }
 }
