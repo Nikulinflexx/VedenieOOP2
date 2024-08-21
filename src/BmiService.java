3027ab84b7b1add10a955d1af2232e905104c5da
Nikita <nickita.nikulin2015@yandex.ru>
2024-08-21 22:52:29 +0300

    modified2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public float calculate(float weight, float height) {
+    public int calculate(float weight, float height) {
         float bmi = weight / (height * height);
-        return bmi;
+        return (int) bmi;
     }
 }
